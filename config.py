c8fa503aed6b48a4cd6266701d06243b9b8c83e6
Gerardo Iuliano <gerardoiuliano99@gmail.com>
2025-05-21 15:40:43 +0200

    add config.py file

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+ETHERSCAN_API_KEY = "your_etherscan_api_key_here"
+ETHERSCAN_API_URL = "https://api.etherscan.io/api"
+ETHERSCAN_RATE_LIMIT_DELAY = 0.25  # In secondi
+
+INFURA_API_KEY = "your_infura_api_key_here"
+
+COUNTER_LIMIT = {
+    "0_4": 1000,
+    "0_5": 1000,
+    "0_6": 1000,
+    "0_7": 1000,
+    "0_8": 1000,
+}
